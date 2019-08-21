f3484260db68f1ce1ea7efa955ea5d1d56baed14
Fengwangdong Zhang <fengwang@fengmac.dyndns.cern.ch>
2019-08-21 15:00:20 +0200

    fix a timer bug

--- Sources/MuTauAnalyzer.cc
+++ Sources/MuTauAnalyzer.cc
@@ -5,7 +5,6 @@
 #include <TStyle.h>
 #include <TCanvas.h>
 #include <TFile.h>
-#include <sys/time.h>
 #include <iomanip>
 #include <iostream>
 #include <TLorentzVector.h>
@@ -24,36 +23,11 @@ void MuTauAnalyzer::Loop()
 
    Long64_t nbytes = 0, nb = 0;
 
-   struct timeval t0;
-   gettimeofday(&t0, 0);
-   int mess_every_n =  std::min(1000000LL, nentries/10);
-
    for (Long64_t jentry=0; jentry<nentries; jentry++) {
       Long64_t ientry = LoadTree(jentry);
       if (ientry < 0) break;
 
-      if (jentry % mess_every_n == 0 && jentry > 0){
-          timeval t1;
-          gettimeofday(&t1, 0);
-          double dt = (t1.tv_sec - t0.tv_sec) + 1.e-6*(t1.tv_usec - t0.tv_usec);
-          dt *= double(nentries  - jentry) / mess_every_n;
-          int dt_s = int(dt + 0.5);
-          int dt_h = int(dt_s) / 3600; 
-          dt_s -= dt_h * 3600;
-          int dt_m = dt_s / 60;
-          dt_s -= dt_m *60;
-          cout << TString::Format("%4.1f%%", (100. * jentry) / nentries)
-              << "\t" << std::setw(11) << jentry << " / " << nentries
-              << "\t " << std::setw(4) << int(dt / mess_every_n * 1.e6 + 0.5) << " us/event"
-              << "\t Remaining time for this dataset loop: " 
-              << std::setw(2) << dt_h << " h "
-              << std::setw(2) << dt_m << " min "
-              << std::setw(2) << dt_s << " s"
-              << "\r" << std::flush;
-          t0 = t1;
-      } // end if for timer settings
-
-      //cout << "*** iEntry: " << jentry << endl;
+      if (jentry % 1000 == 0 && jentry > 0) cout << "*** Processing #Events: " << jentry << endl;
       nb = fChain->GetEntry(jentry);
       nbytes += nb;
 
