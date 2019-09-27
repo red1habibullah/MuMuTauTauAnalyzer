d1e19aa7b2489bd864c664cba9af3b4281375c0a
Fengwangdong Zhang <fengwang@fengmac.dyndns.cern.ch>
2019-09-27 17:38:09 +0200

    add wjets connfig

--- runFakeTauAnalyzer.cc
+++ runFakeTauAnalyzer.cc
@@ -132,5 +132,36 @@ int main(int argc, char **argv)
     // --- always need to reinitialize the weight parameter for new sample -----
     summedWeights = 0;
 
+
+    if (doWhat == "WJETS" || doWhat == "ALL")
+    {
+        if (inputFile.EndsWith(".root"))
+        {
+            lumiana WJetsLumi(inputFile);
+            summedWeights = WJetsLumi.Loop();
+            FakeTauAnalyzer WJetsHist(inputFile, outputDir, lumi*61526.7*1000, summedWeights, maxEvents, true, tauMVAIsoRawORWP, tauMVAIsoRawThreshold, tauMVAIsoWP);
+            WJetsHist.Loop();
+        } // end if inputFile.EndsWith(".root")
+
+        else{
+            ifstream finLumi;
+            finLumi.open(inputFile);
+            string fileName;
+            while (getline(finLumi, fileName))
+            {
+                lumiana WJetsLumi(fileName);
+                summedWeights += WJetsLumi.Loop();
+            } // end while loop on weight sum
+
+            ifstream finTree;
+            finTree.open(inputFile);
+            while (getline(finTree, fileName))
+            {
+                FakeTauAnalyzer WJetsHist(fileName, outputDir, lumi*61526.7*1000, summedWeights, maxEvents, true, tauMVAIsoRawORWP, tauMVAIsoRawThreshold, tauMVAIsoWP);
+                WJetsHist.Loop();
+            } // end while loop on input file list
+        } // end else
+    } // end if WJets
+
     return 0;
 }
