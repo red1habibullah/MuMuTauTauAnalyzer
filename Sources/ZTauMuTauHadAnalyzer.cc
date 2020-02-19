7af3c95ce170f34a9fbb09524facebfcc664b4f5
Fengwangdong Zhang <fengwang@fengwandongsmbp.home>
2020-02-19 20:59:26 +0100

    fix mu isolation option in tauID study

--- Sources/ZTauMuTauHadAnalyzer.cc
+++ Sources/ZTauMuTauHadAnalyzer.cc
@@ -46,7 +46,7 @@ void ZTauMuTauHadAnalyzer::Loop()
       bool findMu1 = false;
       for (unsigned int iMuon=0; iMuon<recoMuonPt->size(); iMuon++)
       {
-          if (recoMuonTriggerFlag->at(iMuon) == 1 && recoMuonIsolation->at(iMuon) < 0.25)
+          if (recoMuonTriggerFlag->at(iMuon) == 1 && ((!invertedMu1Iso && recoMuonIsolation->at(iMuon) < Mu1IsoThreshold) || (invertedMu1Iso && recoMuonIsolation->at(iMuon) > Mu1IsoThreshold)))
           {
               Mu.SetPtEtaPhiE(recoMuonPt->at(iMuon), recoMuonEta->at(iMuon), recoMuonPhi->at(iMuon), recoMuonEnergy->at(iMuon));
               indexMu = iMuon;
